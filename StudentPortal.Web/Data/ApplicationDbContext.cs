39c16ade76ffb8fa336104d4bad6a833f4a14482
Ravi Padache <42465089+ravi357767@users.noreply.github.com>
2025-01-01 01:45:40 +0530

    Added Changes on Files

---
+++ StudentPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Models.Entity;
+
+namespace StudentPortal.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- StudentPortal.Web/Program.cs
+++ StudentPortal.Web/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("StudentPortal")
+    ));
 
 var app = builder.Build();
 
