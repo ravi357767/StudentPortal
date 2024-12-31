6ac53c7615d3fef934e20eb1af9aebea506cc013
Ravi Padache <42465089+ravi357767@users.noreply.github.com>
2025-01-01 02:36:57 +0530

    Added new Files

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
